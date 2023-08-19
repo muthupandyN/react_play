5e325de5ff76cb55fb7fc6dc2a0bc22865bcd1bf
muthupandyN <muthupandynagendran@gmail.com>
2023-08-19 22:48:44 +0530

    props and drilling logic added in todo app

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,11 +1,50 @@
 import Header from './Header';
 import  Footer  from './Footer';
 import Content from './Content';
+import React, { useState } from 'react'
+
 function App() {
+    const [items, setItems] = useState(
+        [
+            {
+                id:1,
+                checked:true,
+                item:"practice react"
+            },
+            {
+                id:2,
+                checked:true,
+                item:"practice python"
+            },
+            {
+                id:3,
+                checked:true,
+                item:"learn investment"
+            }
+        ]
+    )
+
+    const handleCheck= (id) => {
+       const listItem = items.map((item)=>
+       item.id === id ? {...item, checked:!item.checked} :item
+       )
+       setItems(listItem)
+       localStorage.setItem("todo_list",JSON.stringify(listItem))
+    }
+
+    const handleDelete = (id) =>{
+        const listItem = items.filter((item)=>item.id !== id)
+        setItems(listItem)
+        localStorage.setItem("todo_list",JSON.stringify(listItem))
+    }
   return (
     <div className="App">
-      <Header/>
-      <Content/>
+      <Header />
+      <Content 
+      items={items}
+      handleCheck = {handleCheck}
+      handleDelete = {handleDelete}
+      />
       <Footer/>
     </div>
   );
--- todo/src/Content.js
+++ todo/src/Content.js
@@ -1,69 +1,19 @@
-import React, { useState } from 'react'
-
-import { FaTrashAlt} from "react-icons/fa";
 
 
+import ItemList from "./ItemList";
 
-const Content = () => {
 
-    const [items, setItems] = useState(
-        [
-            {
-                id:1,
-                checked:true,
-                item:"practice react"
-            },
-            {
-                id:2,
-                checked:true,
-                item:"practice python"
-            },
-            {
-                id:3,
-                checked:true,
-                item:"learn investment"
-            }
-        ]
-    )
+const Content = ({items, handleCheck, handleDelete}) => {
 
-    const handleCheck= (id) => {
-       const listItem = items.map((item)=>
-       item.id === id ? {...item, checked:!item.checked} :item
-       )
-       setItems(listItem)
-       localStorage.setItem("todo_list",JSON.stringify(listItem))
-    }
-
-    const handleDelete = (id) =>{
-        const listItem = items.filter((item)=>item.id !== id)
-        setItems(listItem)
-        localStorage.setItem("todo_list",JSON.stringify(listItem))
-    }
+   
   return (
    <main >
     {(items.length) ?(
-      <ul>
-        {items.map((item)=>(
-            <li className='item' key={item.id}> 
-                <input 
-                    onChange={()=>handleCheck(item.id)}
-                    type='checkbox'
-                    checked={item.checked}
-                />
-                <label
-                style={(item.checked) ?{
-                    textDecoration : 'line-through'
-                } : null}
-                onDoubleClick={()=>handleCheck(item.id)}
-                >{item.item}</label>
-                <FaTrashAlt
-                  onClick={()=>handleDelete(item.id)}
-                  role='button'
-                  tabIndex="0"
-                />
-            </li>
-        ))}
-      </ul>
+       <ItemList
+        items = {items}
+        handleCheck = {handleCheck}
+        handleDelete = {handleDelete}
+        />
       ) : (
       <p>List is empty</p>
      )
